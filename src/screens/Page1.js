126800013e69aa0b13f51e57779dc21a80f26e23
chinnawutPong <chinnawut.k.@ku.th>
2021-04-22 15:19:52 +0700

    edit header

--- src/screens/Page1.js
+++ src/screens/Page1.js
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
